83ac540d0a78f4c53301b647937114f4dfe5c1dd
WonHyung Shin <wonhyung64@icloud.com>
2022-10-20 21:17:51 +0900

    feat - reconstruction

--- run.py
+++ run.py
@@ -507,9 +507,6 @@ class FCOS:
             name=name
         )
 
-#%%
-
-
 # %%
 lr = 0.01
 batch_size = 8
@@ -594,12 +591,8 @@ trainset_provider = {
 }
 fcos = FCOS(config, trainset_provider)
 fcos()
-#%%
-tf.keras.initializers.VarianceScaling()
-
 
 #%%
-from tensorflow.keras.models import Model
 
 def _detect_head(bottom, num_classes):
     conv1 = _bn_activation_conv(bottom, 256, 3, 1)
@@ -617,31 +610,7 @@ def _detect_head(bottom, num_classes):
 
     return pconf, preg, pcenterness
 
-def build_head(output_filters, bias_init):
-    head = tf.keras.Sequential([tf.keras.Input(shape=[None, None, 256])])
-    kernel_init = tf.initializers.RandomNormal(0.0, 0.01)
-    for _ in range(4):
-        head.add(
-            Conv2D(256, 3, padding="same", kernel_initializer=kernel_init)
-        )
-        head.add(ReLU())
 
-    head.add(
-        Conv2D(
-            output_filters,
-            3,
-            1,
-            padding="same",
-            kernel_initializer=kernel_init,
-            bias_initializer=bias_init,
-        )
-    )
-    if output_filters != 4:
-        head.add(
-            Lambda(lambda x: [x[..., :-1], x[..., -1:]])
-        )
-
-    return head
 
 
 def _get_pyramid( feat, feature_size, top_feat=None):
@@ -736,8 +705,11 @@ def _avg_pooling( bottom, pool_size, strides, name=None):
 #         name=name
 #     )
 
+#%%
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Conv2D, ReLU, Lambda, Layer
 class FCOS(Model):
-    def __init__(self, config, data_provider, **kwargs):
+    def __init__(self, config, data_provider, backbone=None, **kwargs):
         super(FCOS, self).__init__(name="FCOS", **kwargs)
         self.config = config
         self.data_provider = data_provider
@@ -751,91 +723,131 @@ class FCOS(Model):
         self.nms_score_threshold = config['nms_score_threshold']
         self.nms_max_boxes = config['nms_max_boxes']
         self.nms_iou_threshold = config['nms_iou_threshold']
+        self.fpn = FeaturePyramid(backbone)
 
-        self.is_bottleneck = True,
-        self.block_list = [3, 4, 6, 3]     # must len 4
-        self.filters_list = [16 * (2 ** i) for i in range(len(self.block_list))]
-
-        if self.mode == 'train':
-            self.num_train = data_provider['num_train']
-            self.num_val = data_provider['num_val']
-            self.train_generator = data_provider['train_generator']
-            self.train_initializer, self.train_iterator = self.train_generator
-            if data_provider['val_generator'] is not None:
-                self.val_generator = data_provider['val_generator']
-                self.val_initializer, self.val_iterator = self.val_generator
+        prior_probability = tf.constant_initializer(-np.log((1 - 0.01) / 0.01))
+        self.cls_head = build_head(self.num_classes + 1, prior_probability)
+        self.box_head = build_head(4, "zeros")
+        self.divider = Lambda(lambda x: [x[..., :-1], x[..., -1:]])
 
 
     @tf.function
-    def call(self, image):
-        endpoints = []
-        conv1_1 = _conv_bn_activation(
-            bottom=image,
-            filters=16,
-            kernel_size=7,
-            strides=2,
+    def call(self, image, training=False):
+        features = self.fpn(image, training=training)
+        N = tf.shape(image)[0]
+        cls_outputs = []
+        ctr_outputs = []
+        box_outputs = []
+        for feature in features:
+            box_outputs.append(tf.reshape(self.box_head(feature), [N, -1, 4]))
+            cls_output = self.cls_head(feature)
+            cls_output, ctr_output = self.divider(cls_output)
+            cls_outputs.append(
+                tf.reshape(cls_output, [N, -1, self.num_classes])
+            )
+            ctr_outputs.append(
+                tf.reshape(ctr_output, [N, -1, 1])
+            )
+        cls_outputs = tf.concat(cls_outputs, axis=1)
+        ctr_outputs = tf.concat(ctr_outputs, axis=1)
+        box_outputs = tf.concat(box_outputs, axis=1)
+
+        return (box_outputs, cls_outputs, ctr_outputs)
+
+
+def get_backbone():
+    backbone = tf.keras.applications.ResNet50(
+        include_top=False, input_shape=[None, None, 3]
+    )
+    c3_output, c4_output, c5_output = [
+        backbone.get_layer(layer_name).output
+        for layer_name in ["conv3_block4_out", "conv4_block6_out", "conv5_block3_out"]
+    ]
+
+    return tf.keras.Model(
+        inputs=[backbone.inputs], outputs=[c3_output, c4_output, c5_output]
+    )
+
+class FeaturePyramid(Layer):
+
+    def __init__(self, backbone=None, **kwargs):
+        super(FeaturePyramid, self).__init__(name="FeaturePyramid", **kwargs)
+        self.backbone = backbone if backbone else get_backbone()
+        self.conv_c3_1x1 = Conv2D(256, 1, 1, "same")
+        self.conv_c4_1x1 = Conv2D(256, 1, 1, "same")
+        self.conv_c5_1x1 = Conv2D(256, 1, 1, "same")
+        self.conv_c3_3x3 = Conv2D(256, 3, 1, "same")
+        self.conv_c4_3x3 = Conv2D(256, 3, 1, "same")
+        self.conv_c5_3x3 = Conv2D(256, 3, 1, "same")
+        self.conv_c6_3x3 = Conv2D(256, 3, 2, "same")
+        self.conv_c7_3x3 = Conv2D(256, 3, 2, "same")
+        self.upsample_2x = CustomUpSampling2D(2)
+
+    @tf.function
+    def call(self, images, training=False):
+        c3_output, c4_output, c5_output = self.backbone(images, training=training)
+        p3_output = self.conv_c3_1x1(c3_output)
+        p4_output = self.conv_c4_1x1(c4_output)
+        p5_output = self.conv_c5_1x1(c5_output)
+        p4_output = p4_output + self.upsample_2x(p5_output)
+        p3_output = p3_output + self.upsample_2x(p4_output)
+        p3_output = self.conv_c3_3x3(p3_output)
+        p4_output = self.conv_c4_3x3(p4_output)
+        p5_output = self.conv_c5_3x3(p5_output)
+        p6_output = self.conv_c6_3x3(c5_output)
+        p7_output = self.conv_c7_3x3(tf.nn.relu(p6_output))
+
+        return p3_output, p4_output, p5_output, p6_output, p7_output
+
+class CustomUpSampling2D(tf.keras.layers.Layer):
+    def __init__(self, size):
+        super(CustomUpSampling2D, self).__init__()
+        if type(size) is not tuple and type(size) is not list:
+            size = (size, size)
+        self.size = size
+
+    def build(self, input_shape):
+        pass
+
+    def call(self, input):
+        return tf.repeat(tf.repeat(input, self.size[0], axis=1), self.size[1], axis=2)
+
+
+def build_head(output_filters, bias_init):
+    head = tf.keras.Sequential([tf.keras.Input(shape=[None, None, 256])])
+    kernel_init = tf.initializers.RandomNormal(0.0, 0.01)
+    for _ in range(4):
+        head.add(
+            Conv2D(256, 3, padding="same", kernel_initializer=kernel_init)
         )
-        pool1 = _max_pooling(
-            bottom=conv1_1,
-            pool_size=3,
-            strides=2,
-            name='pool1'
+        head.add(ReLU())
+    head.add(
+        Conv2D(
+            output_filters,
+            3,
+            1,
+            padding="same",
+            kernel_initializer=kernel_init,
+            bias_initializer=bias_init,
         )
-        if self.is_bottleneck:
-            stack_residual_unit_fn = _residual_bottleneck
-        else:
-            stack_residual_unit_fn = _residual_block
-        residual_block = pool1
-        for i in range(self.block_list[0]):
-            residual_block = stack_residual_unit_fn(residual_block, self.filters_list[0], 1, 'block1_unit' + str(i + 1))
-        endpoints.append(residual_block)
-        for i in range(1, len(self.block_list)):
-            residual_block = stack_residual_unit_fn(residual_block, self.filters_list[i], 2, 'block' + str(i + 1) + '_unit' + str(1))
-            for j in range(1, self.block_list[i]):
-                residual_block = stack_residual_unit_fn(residual_block, self.filters_list[i], 1, 'block' + str(i + 1) + '_unit' + str(j + 1))
-            endpoints.append(residual_block)
+    )
 
-        c3 = _bn_activation_conv(endpoints[-3], 256, 1, 1)
-        c4 = _bn_activation_conv(endpoints[-2], 256, 1, 1)
-        c5 = _bn_activation_conv(endpoints[-1], 256, 1, 1)
-        p5 = _get_pyramid(c5, 256)
-        p4, top_down = _get_pyramid(c4, 256, p5)
-        p3, _ = _get_pyramid(c3, 256, top_down)
-        p6 = _bn_activation_conv(p5, 256, 3, 2)
-        p7 = _bn_activation_conv(p6, 256, 3, 2)
-
-        p3conf, p3reg, p3center = _detect_head(p3, self.num_classes)
-        p4conf, p4reg, p4center = _detect_head(p4, self.num_classes)
-        p5conf, p5reg, p5center = _detect_head(p5, self.num_classes)
-        p6conf, p6reg, p6center = _detect_head(p6, self.num_classes)
-        p7conf, p7reg, p7center = _detect_head(p7, self.num_classes)
-
-        s3, s4, s5, s6, s7 = 8, 16, 32, 64, 128
-        p3shape = [tf.shape(p3center)[1], tf.shape(p3center)[2]]
-        p4shape = [tf.shape(p4center)[1], tf.shape(p4center)[2]]
-        p5shape = [tf.shape(p5center)[1], tf.shape(p5center)[2]]
-        p6shape = [tf.shape(p6center)[1], tf.shape(p6center)[2]]
-        p7shape = [tf.shape(p7center)[1], tf.shape(p7center)[2]]
-        h3 = tf.range(0., tf.cast(p3shape[0], tf.float32), dtype=tf.float32)
-        w3 = tf.range(0., tf.cast(p3shape[1], tf.float32), dtype=tf.float32)
-        h4 = tf.range(0., tf.cast(p4shape[0], tf.float32), dtype=tf.float32)
-        w4 = tf.range(0., tf.cast(p4shape[1], tf.float32), dtype=tf.float32)
-        h5 = tf.range(0., tf.cast(p5shape[0], tf.float32), dtype=tf.float32)
-        w5 = tf.range(0., tf.cast(p5shape[1], tf.float32), dtype=tf.float32)
-        h6 = tf.range(0., tf.cast(p6shape[0], tf.float32), dtype=tf.float32)
-        w6 = tf.range(0., tf.cast(p6shape[1], tf.float32), dtype=tf.float32)
-        h7 = tf.range(0., tf.cast(p7shape[0], tf.float32), dtype=tf.float32)
-        w7 = tf.range(0., tf.cast(p7shape[1], tf.float32), dtype=tf.float32)
-        [grid_x3, grid_y3] = tf.meshgrid(w3, h3)
-        [grid_x4, grid_y4] = tf.meshgrid(w4, h4)
-        [grid_x5, grid_y5] = tf.meshgrid(w5, h5)
-        [grid_x6, grid_y6] = tf.meshgrid(w6, h6)
-        [grid_x7, grid_y7] = tf.meshgrid(w7, h7)
-
-        return (p3conf, p3reg, p3center) 
+    return head
 # %%
-data = tf.ones(shape=[8,512,512,3], dtype=tf.float32)
+img, gt = train_set[1].get_next()
+
 fcos = FCOS(config, trainset_provider)
-fcos(data)
+predict = fcos(img)
 fcos.summary()
 fcos
+
+#%%
+import math
+img_size = [512, 512]
+math.prod(img_size)
+gt_h = gt[..., 2] - gt[..., 0] 
+gt_w = gt[..., 3] - gt[...,1]
+gt_size = tf.sqrt(gt_h * gt_w * math.prod(img_size))
+idx = gt_size < 64
+gt
+gt[idx]
