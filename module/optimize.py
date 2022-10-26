c8983e2a8f36922a2df890d8164b3e504fb3ef33
WonHyung Shin <wonhyung64@icloud.com>
2022-10-26 18:59:38 +0900

    feat - experiment1

--- module/optimize.py
+++ module/optimize.py
@@ -2,8 +2,8 @@ import tensorflow as tf
 
 
 def build_optimizer(batch_size, data_num, momentum):
-    boundaries = [data_num // batch_size * epoch for epoch in (1, 50, 60, 70)]
-    values = [1e-5, 1e-3, 1e-4, 1e-6, 1e-7]
+    boundaries = [data_num // batch_size * epoch for epoch in (60, 80)]
+    values = [1e-2, 1e-3, 1e-4]
     learning_rate_fn = tf.optimizers.schedules.PiecewiseConstantDecay(
         boundaries=boundaries, values=values
     )
